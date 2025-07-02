f5e2f2e2879511801045c25913c1e794fed7ebec
19VISHAL98 <vishalkumar8673@gmail.com>
2025-07-02 16:54:03 +0530

    hii

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>My Website</h1>
+        <h1>My Portfolio</h1>
         <nav>
             <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
         </nav>
